18309ed66b72d9db25781b8d2b3b7c3ffb029559
moussaoui1228 <moussaouisarah78@gmail.com>
2026-03-23 13:32:28 +0100

    add the models

---
+++ backend/server/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require ("mongoose")
+
+const userSchema = new mongoose.Schema({
+    first_name: { type: String, required: true, trim: true },
+    last_name: { type: String, required: true, trim: true },
+    phone: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    address: { type: String, trim: true },
+    role: { type: String, enum: ['customer', 'owner'], default: 'customer' },
+    is_subscribed: { type: Boolean, default: false },
+    
+    //those we need them when i do the auth/forget_password route 
+    //that why i added them
+    reset_password_code: { type: String },
+    reset_password_expires: { type: Date },
+}, 
+{timestamps:true ,});
+module.exports = mongoose.model('user' , userSchema) ;
